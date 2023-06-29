3cf86c9912949455659c7a5745f1a41f789bb3c9
pabloJulianRial <rialpabloj@hotmail.com>
2023-06-29 13:09:08 +0100

    deploying and hosting

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const app = express();
 const { getTopics } = require("./controllers/ncnews.controllers");
 const { getEndpoints } = require("./controllers/ncnews.controllers");
 const { getArticleById } = require("./controllers/ncnews.controllers");
 const { getArticles } = require("./controllers/ncnews.controllers");
-const app = express();
 
 app.get("/api", getEndpoints);
 
